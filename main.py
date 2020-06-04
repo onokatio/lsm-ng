ece7686e0936ae09ef16e2ba95866fa709ddec21
onokatio <onokatio@gmail.com>
2020-06-05 00:43:26 +0900

    use dimensionN arrays

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy
 import pandas
 import matplotlib.pyplot as pyplot
 
-dimensionN = 3
+dimensionN = 1
 
 train_data = pandas.read_csv("data/lsmCompe_train.csv",header=None)
 test_data = pandas.read_csv("data/lsmCompe_test.csv",header=None)
@@ -15,10 +15,10 @@ sum_xy[n] == sum(x^n * y)
 sum_x = []
 sum_xy = []
 
-for i in range(dimensionN * 2):
+for i in range(dimensionN * 2 + 1):
     sum_x.insert(i, sum(train_data.loc[:,0].values ** i))
 
-for i in range(dimensionN):
+for i in range(dimensionN + 1):
     sum_xy.insert(i, sum((train_data.loc[:,0].values ** i) * train_data.loc[:,1].values))
 
 #sum_x2 = sum(train_data.loc[:,0].values ** 2)
@@ -32,11 +32,23 @@ sum_1 = train_data.index.stop
 #print(sum_y)
 #print(sum_xy)
 
-left_matrix = numpy.matrix([[sum_x[2], sum_x[1]]
-                           ,[sum_x[1],  sum_x[0]]])
-right_matrix = numpy.matrix([[sum_xy[1]],
-                             [sum_xy[0]]])
+left_matrix = []
+right_matrix = []
 
+for i in range(dimensionN + 1):
+    item = []
+    for j in range(dimensionN + 1):
+        item.insert(j, sum_x[2*dimensionN - i - j])
+    left_matrix.insert(i, item)
+
+for i in range(dimensionN + 1):
+    right_matrix.insert(j, [sum_xy[dimensionN - i]])
+
+left_matrix = numpy.matrix(left_matrix)
+right_matrix = numpy.matrix(right_matrix)
+
+print(left_matrix)
+print(right_matrix)
 (a,b) = left_matrix.I @ right_matrix
 (a,b) = (a.item(),b.item())
 
