43566a741ef61fe34c5d87dfe6facd128ec6466a
onokatio <onokatio@gmail.com>
2020-06-05 05:46:17 +0900

    export final w

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ global_w = []
 index = 0;
 
 def f(x,w):
-    return numpy.matrix(numpy.flip(numpy.logspace(0, dimensionN, dimensionN + 1, base=x))) * w
+    return sum(numpy.matrix(numpy.flip(numpy.logspace(0, dimensionN, dimensionN + 1, base=x))) * w)
 
 def learning(train_index, test_index):
     sum_x = []
@@ -65,6 +65,7 @@ def learning(train_index, test_index):
     print("left_matrix=", left_matrix)
     print("right_matrix=", right_matrix)
     w = left_matrix.I @ right_matrix
+    print(w)
     #print("w=", w)
     global_w.insert(index, w)
 
@@ -72,7 +73,7 @@ def learning(train_index, test_index):
     #pyplot.plot(original_train_data[:,0],original_train_data[:,1],'ro')
     pyplot.plot(train_data[train_index,0],train_data[train_index,1],'ro')
     #pyplot.plot(outlier_train_data[train_index,0],outlier_train_data[train_index,1],'bo')
-    pyplot.plot(train_data[test_index,0],train_data[test_index,1],'go')
+    #pyplot.plot(train_data[test_index,0],train_data[test_index,1],'go')
 
     frange = numpy.arange(0,original_train_data[:,0].max(),1)
 
@@ -93,11 +94,27 @@ for train_index, test_index in skf.split(original_train_data[:,0],original_train
 print(global_w)
 final_w = []
 for i in range(dimensionN + 1):
-    final_w.insert(i, 0)
+    final_w.insert(i, [0])
     for j in range(k_closs_validation):
-        final_w[i] += global_w[j][i]
-    final_w[i] /= k_closs_validation
-    print("final_w[", i, "]=",final_w[i].item())
+        print("now w is", global_w[j][i])
+        final_w[i][0] += global_w[j][i].item()
+    final_w[i][0] /= k_closs_validation
+    #final_w[i][0] = final_w[i].item()
+    #print("final_w[", i, "]=",final_w[i].item())
 
+#final_w2 = numpy.array(final_w, dtype=numpy.float128)
+print(final_w)
+print(numpy.flip(numpy.logspace(0, dimensionN, dimensionN + 1, base=2)))
+
+frange = numpy.arange(0,original_train_data[:,0].max(),1)
+
+
+ans = []
+
+for i in frange:
+    ans.append( f(i,final_w).item() )
+    #ans.append(numpy.flip(numpy.logspace(0, dimensionN, dimensionN + 1, base=i)) * final_w)
+
+pyplot.plot(frange,ans)
 
 pyplot.show()
